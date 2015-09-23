2120cad3da118ab2f6cc57c19f5a11ad3cadf6d3
ryanau <ryan.au@hotmail.com>
2015-09-22 19:10:42 -0700

    set up rails api skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+EasyBooks::Application.config.secret_token = '19664020490423a5893090e38e771ee4348de48930598959f24678add9d2162f1ac733de519aa0d38ab125931482d971676c1d7573cdfbc9aba7a611b11282b7'
