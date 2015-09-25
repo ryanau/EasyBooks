61f6cad3a42c3c182f9cd5e94d3c1cba230463f2
ryanau <ryan.au@hotmail.com>
2015-09-25 13:13:41 -0700

    add card object with picture overlay + moment.js

--- client/components/PublicPost.jsx
+++ client/components/PublicPost.jsx
@@ -2,9 +2,19 @@ var React = require('react');
 var $ = require('jquery');
 var Router = require('react-router');
 var Link = Router.Link;
+var moment = require('moment');
 
 var mui = require('material-ui');
 var ThemeManager = new mui.Styles.ThemeManager();
+var Card = mui.Card;
+var CardHeader = mui.CardHeader;
+var CardText = mui.CardText;
+var CardActions = mui.CardActions;
+var CardTitle = mui.CardTitle;
+var CardMedia = mui.CardMedia;
+var Snackbar = mui.Snackbar;
+var FlatButton = mui.FlatButton;
+
 
 PublicPost = React.createClass({
 	childContextTypes: {
@@ -19,7 +29,12 @@ PublicPost = React.createClass({
   	var post = this.props.post
   	return (
   		<div>
-  			<h4>{post.title}</h4>
+  			<Card key={post.id} initiallyExpanded={false}>
+  				<CardMedia overlay={
+  					<CardTitle title={post.title} subtitle={"$" + post.price + " | Created " + moment(post.created_at).fromNow()} />}>
+  					<img src={post.picture_url} />
+  				</CardMedia>
+  			</Card>
   		</div>
   	)
   }
